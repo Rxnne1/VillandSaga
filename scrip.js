5da6bd80f49cf0e67a60e15eb970e53b4d92b535
Rxnne <reneedanielee@gmail.com>
2026-01-26 19:42:36 -0500

    Se añaden los archivos iniciales

---
+++ scrip.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menuToggle");
+const menu = document.querySelector(".menu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
